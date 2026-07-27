f642fdba3d4b78a0aa004bf2212e10f5b505e2f4
R.P.Nishwa <nishwapandiyan@gmail.com>
2026-07-27 11:27:57 +0530

    ✅ Set .intersection() Operation [python3] — approach1

---
+++ solutions/python3/easy/py-set-intersection-operation/solution-approach1.py
@@ -0,0 +1,17 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/py-set-intersection-operation/problem?isFullScreen=true
+# Problem     Set .intersection() Operation
+# Difficulty  Easy
+# Subdomain   Sets
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-07-09, 11:17 p.m.
+# ──────────────────────────────────────────────────
+
+n = int(input())
+l1 = set(list(map(int,input().split())))
+s = int(input())
+l2 = set(list(map(int,input().split())))
+print(len(l1 & l2))
+
