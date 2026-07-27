3f0f90a0e11dbd7fe0bdb3754b3547f651a2713a
R.P.Nishwa <nishwapandiyan@gmail.com>
2026-07-27 11:39:56 +0530

    ✅ Average Population [mysql] — approach1

---
+++ solutions/mysql/easy/average-population/solution-approach1.sql
@@ -0,0 +1,12 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/average-population/problem?isFullScreen=true
+-- Problem     Average Population
+-- Difficulty  Easy
+-- Subdomain   Aggregation
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-04-07, 01:37 p.m.
+-- ──────────────────────────────────────────────────
+
+SELECT round(avg(population)) from  city;
