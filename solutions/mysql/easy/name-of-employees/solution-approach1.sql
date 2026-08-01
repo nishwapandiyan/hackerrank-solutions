2bd5666c50016c71c6b67a4535459b1347f4ad89
R.P.Nishwa <nishwapandiyan@gmail.com>
2026-08-01 18:04:16 +0530

    ✅ Employee Names [mysql] — approach1

---
+++ solutions/mysql/easy/name-of-employees/solution-approach1.sql
@@ -0,0 +1,16 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/name-of-employees/problem?isFullScreen=true
+-- Problem     Employee Names
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-04-05, 09:11 a.m.
+-- ──────────────────────────────────────────────────
+
+/*
+Enter your query here.
+*/
+
+select name from employee order by name asc; 
