66f0baf6676863a7c742661f1423245872e3db95
R.P.Nishwa <nishwapandiyan@gmail.com>
2026-08-01 18:01:19 +0530

    ✅ Weather Observation Station 1 [mysql] — approach1

---
+++ solutions/mysql/easy/weather-observation-station-1/solution-approach1.sql
@@ -0,0 +1,16 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/weather-observation-station-1/problem?isFullScreen=true
+-- Problem     Weather Observation Station 1
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-04-04, 03:10 p.m.
+-- ──────────────────────────────────────────────────
+
+/*
+Enter your query here.
+*/
+
+SELECT city ,state FROM station;
