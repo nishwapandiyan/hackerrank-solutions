6f9682e66aca45b1212966020fe3c90797f72d7f
R.P.Nishwa <nishwapandiyan@gmail.com>
2026-08-01 18:00:02 +0530

    ✅ Revising the Select Query II [mysql] — approach1

---
+++ solutions/mysql/easy/revising-the-select-query-2/solution-approach1.sql
@@ -0,0 +1,12 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/revising-the-select-query-2/problem?isFullScreen=true
+-- Problem     Revising the Select Query II
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-04-04, 02:52 p.m.
+-- ──────────────────────────────────────────────────
+
+select name from city where population > 120000 and countrycode ='usa';
