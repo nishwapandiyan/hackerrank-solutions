605a34e6dfa3fef67ff049e49fb401b4f82368a5
R.P.Nishwa <nishwapandiyan@gmail.com>
2026-08-01 18:01:07 +0530

    ✅ Japanese Cities' Names [mysql] — approach1

---
+++ solutions/mysql/easy/japanese-cities-name/solution-approach1.sql
@@ -0,0 +1,12 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/japanese-cities-name/problem?isFullScreen=true
+-- Problem     Japanese Cities' Names
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-04-04, 03:04 p.m.
+-- ──────────────────────────────────────────────────
+
+select name from city where countrycode = 'jpn';
